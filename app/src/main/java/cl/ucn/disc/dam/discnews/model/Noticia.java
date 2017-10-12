975468aaa0e5c0353747664a6e7e6e451facc5be
Raos <raos.fast@gmail.com>
2017-10-12 17:23:27 -0300

    Actualizacion de configuracion
    * Java v1.8
    * Exclusion de archivos de librerias externas.
    * Lint.

--- app/src/main/java/cl/ucn/disc/dam/discnews/model/Noticia.java
+++ app/src/main/java/cl/ucn/disc/dam/discnews/model/Noticia.java
@@ -5,4 +5,18 @@ package cl.ucn.disc.dam.discnews.model;
  */
 
 public final class Noticia {
+
+    /**
+     * Descripcion en una linea del titulo de la noticia
+     */
+    private String titulo;
+
+    /**
+     * Descripcion en 2 lineas y con un maximo de 140 chars
+     */
+    private String resumen;
+
+    /**
+     * Contenido completo de la Noticia
+     */
 }
